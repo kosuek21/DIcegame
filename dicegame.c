e2af67b73249dbd65e4b88b82df4b5a892cbbab4
kosuke21 <s1280007@u-aizu.ac.jp>
2021-07-07 18:39:29 +0900

    Greetingsadded

--- dicegame.c
+++ dicegame.c
@@ -5,9 +5,14 @@
 int main(){
   srand((unsigned int)time(NULL));
   int dice1 = 0, dice2 = 0;
+  char name[10];
 
   dice1 = rand() % 6 + 1;
   dice2 = rand() % 6 + 1;
+
+  printf("What is your name?\n>");
+  scanf("%s",&name);
+  printf("Hello, %s!\n",name);
 
   
   printf("Rolling the dice...\n");
